6dd7ec639a53d559e8303c6602b6b94142b84ec8
Steven Haryanto <stevenharyanto@gmail.com>
2012-08-09 05:18:43 +0000

    Import of SHARYANTO/Perinci-Sub-Property-retry-0.04 from CPAN.
    gitpan-cpan-distribution: Perinci-Sub-Property-retry
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         SHARYANTO/Perinci-Sub-Property-retry-0.04.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Perinci/Sub/Property/retry.pm
+++ lib/Perinci/Sub/Property/retry.pm
@@ -6,7 +6,7 @@ use warnings;
 
 use Perinci::Util qw(declare_property);
 
-our $VERSION = '0.03'; # VERSION
+our $VERSION = '0.04'; # VERSION
 
 declare_property(
     name => 'retry',
@@ -121,7 +121,7 @@ Perinci::Sub::Wrapper::Property::retry - Specify automatic retry
 
 =head1 VERSION
 
-version 0.03
+version 0.04
 
 =head1 SYNOPSIS
 
